8323b58ca1d90913ba1b45282d85c89484e6eafd
Criso <68233846+crisoalmoniga@users.noreply.github.com>
2024-01-24 18:31:29 -0300

    Creacion clase pantalla final

--- MAVI_Trabajo_Final/Final.cpp
+++ MAVI_Trabajo_Final/Final.cpp
@@ -1,1 +1,12 @@
 #include "Final.h"
+
+Final::Final() {
+
+    //Carga de las texturas y configuracion de los sprites para las pantallas
+
+    texturaPerdiste.loadFromFile("perdiste.png");
+    spritePerdiste.setTexture(texturaPerdiste);
+
+    texturaGanaste.loadFromFile("ganaste.png");
+    spriteGanaste.setTexture(texturaGanaste);
+}
--- MAVI_Trabajo_Final/Final.h
+++ MAVI_Trabajo_Final/Final.h
@@ -1,5 +1,17 @@
 #pragma once
-class Final
-{
-};
+#include <SFML/Graphics.hpp>
 
+class Final {
+
+public:
+
+    Final();
+
+    sf::Texture texturaPerdiste;
+    sf::Sprite spritePerdiste;
+    sf::Texture texturaGanaste;
+    sf::Sprite spriteGanaste
+
+    sf::RenderWindow* ventana;
+
+};
